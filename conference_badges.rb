f2b68ed80b4b1020b57c926012f787ec1b5e64d0
Mischka Booley <mischka@gearbox.com.au>
2022-08-04 15:18:26 +1000

    commit tree bcd3d4dc5406c49c9f46f248aa566cb9d02b0d2a

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  new_array = []
+  names.each do |name|
+   new_array.push("Hello, my name is #{name}.")
+  end
+  return new_array
+end
+
+def assign_rooms(speakers)
+  speakers.map.with_index(1) do |speaker, index|
+    "Hello, #{speaker}! You'll be assigned to room #{index}!"
+  end
+end
+
+def printer(names)
+  batch_badge_creator(names).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(names).each do |assignment|
+    puts assignment
+  end
+end
+
+printer(["Mike", "Sam", "Erin", "Nancy"])
